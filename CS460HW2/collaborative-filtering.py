a1c42f87c4a8d5f2f9b24a5b136d9dab6524b7d9
Rupal Shah <rupal.shah@uky.edu>
2018-02-13 20:43:51 -0500

    creates list of each user and their rating for each movie (default=2.5)

--- CS460HW2/collaborative-filtering.py
+++ CS460HW2/collaborative-filtering.py
@@ -1,12 +1,41 @@
-import csv
+# NOTES FOR DOCUMENTATION WRITE-UP:
+# Assumptions:
+#   943 users
+#   1682 movies
+#   column 1 - user, 2 - movie, 3 - rating
+#   if user has not seen one of the movies, I set a default rating of 2.5 (half on the 1-5 rating scale)
 
-# read in training data
-# separate columns and store
-    # 1st = user
-    # 2nd = movie
-    # 3rd = rating
-    # 4th = ???
-# ask for user input for person & movie? do we ask for a rating guess? output rating for movie? email brent
+# --------------------------------------- FUNCTIONS --------------------------------------- #
+
+
+# output list of cosine similarities for each piece of data
+def cosine_similarity(person_num, movie_num, training_data):
+
+    person_movie_data = []  # create list for each user and their rating for each movie
+    similarity_ratios = []
+
+    # for each user
+    for i in range(1, 944):
+
+        temp = [2.5]*1683  # index 0 = user, 1-1682 = rating of each movie (default value = 2.5)
+        temp[0] = i
+
+        # create a list with a rating for each movie
+        for row in training_data:
+            if int(row[0]) == i:
+                 temp[int(row[1])] = int(row[2])
+
+        print(temp)
+        person_movie_data.append(temp)
+
+
+    # for user in person_movie_data:
+    #     print(user)
+
+    return similarity_ratios
+
+
+# ----------------------------------------- MAIN ----------------------------------------- #
 
 
 # open training data file
@@ -19,14 +48,15 @@ for line in training_file_contents.splitlines():
     training_rows.append(line.split())
 
 
-# open item data file (specifies the genres of each movie)
-item_file = open("data/u-item.item", "r")
-
-# store each row in a list
-item_reader = csv.reader(item_file, delimiter='|')
-item_rows = list(item_reader)
-
 # ask user for the person number & movie number they would like to use to predict the rating
 person = input("Please input the number of the person you are interested in: ")
 movie = input("Please input the number of the movie you are interested in: ")
 
+# calculate cosine similarity for each piece of data in training
+similarities = cosine_similarity(person, movie, training_rows)
+
+# for now k = 3
+# find 3 highest cosine similarity to given person & movie
+
+# k = 3
+# k_nearest_neighbors(k, similarities)
